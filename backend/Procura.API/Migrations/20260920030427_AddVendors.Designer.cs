9d55bf24f687eba0eb0b45c9e620814a2c30c7eb
IT24300049 <It24300049@my.sliit.lk>
2026-09-20 08:36:43 +0530

    Add AddVendors migration

--- backend/Procura.API/Migrations/20260920030427_AddVendors.Designer.cs
+++ backend/Procura.API/Migrations/20260920030427_AddVendors.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Procura.API.Shared.Data;
@@ -11,9 +12,11 @@ using Procura.API.Shared.Data;
 namespace Procura.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260920030427_AddVendors")]
+    partial class AddVendors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
